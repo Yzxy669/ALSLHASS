3ac66ec1fe97399351814b41c773f5d887b756fe
Yzxy669 <59546836+Yzxy669@users.noreply.github.com>
2024-05-02 16:56:45 +0800

    Update main.py

--- ALSLHASS/main.py
+++ ALSLHASS/main.py
@@ -10,32 +10,31 @@ if __name__ == '__main__':
     class_num = class_num  # 数据类别个数
     fuse_type = 'AFF'  # 注意力特征融合类型
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    iter_num = 0  # 当前迭代次数
-    while True:
-        Train_iterNum = 'Train-%s' % iter_num
-        # 加载训练数据
-        isbi_dataset = dp.ISBI_Loader(data_path, Train_iterNum, transform=torchvision.transforms.ToTensor())
-        train_loader = torch.utils.data.DataLoader(dataset=isbi_dataset,
-                                                   batch_size=32,
-                                                   shuffle=True)
-        print("训练集个数：", len(isbi_dataset))
+    iter_num = 0  
+    # 加载训练数据
+    Train_iterNum = 'Train-%s' % iter_num
+    isbi_dataset = dp.ISBI_Loader(data_path, Train_iterNum, transform=torchvision.transforms.ToTensor())
+    train_loader = torch.utils.data.DataLoader(dataset=isbi_dataset,
+                                               batch_size=32,
+                                               shuffle=True)
+    print("训练集个数：", len(isbi_dataset))
 
-        isbi_dataset = dp.ISBI_Loader(data_path, 'ValidSet', transform=torchvision.transforms.ToTensor())
-        val_loader = torch.utils.data.DataLoader(dataset=isbi_dataset,
-                                                 batch_size=128,
-                                                 shuffle=False)
-        print("验证集个数：", len(isbi_dataset))
+    isbi_dataset = dp.ISBI_Loader(data_path, 'ValidSet', transform=torchvision.transforms.ToTensor())
+    val_loader = torch.utils.data.DataLoader(dataset=isbi_dataset,
+                                             batch_size=128,
+                                             shuffle=False)
+    print("验证集个数：", len(isbi_dataset))
 
-        # 模型
-        print("开始训练.....")
-        aff_resnet_model = cross_train_verifi(train_loader, class_num, fuse_type, val_loader, data_path, iter_num)
+    # 模型
+    print("开始训练.....")
+    aff_resnet_model = cross_train_verifi(train_loader, class_num, fuse_type, val_loader, data_path, iter_num)
 
-        isbi_dataset = dp.ISBI_Loader(data_path, 'Test', transform=torchvision.transforms.ToTensor())
-        test_loader = torch.utils.data.DataLoader(dataset=isbi_dataset,
-                                                  batch_size=128,
-                                                  shuffle=False)
-        print("测试集个数：", len(isbi_dataset))
-        print("开始测试.....")
-        guide_feature, pre_class_path, features_list = predict(aff_resnet_model, test_loader, class_num, iter_num,
-                                                               data_path)
-        iter_num += 1
+    isbi_dataset = dp.ISBI_Loader(data_path, 'Test', transform=torchvision.transforms.ToTensor())
+    test_loader = torch.utils.data.DataLoader(dataset=isbi_dataset,
+                                              batch_size=128,
+                                              shuffle=False)
+    print("测试集个数：", len(isbi_dataset))
+    print("开始测试.....")
+    guide_feature, pre_class_path, features_list = predict(aff_resnet_model, test_loader, class_num, iter_num,
+                                                           data_path)
+  
