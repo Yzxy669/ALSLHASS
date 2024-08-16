974ace5f214dcce0488f6ca5d89d7275d6b9803b
Yzxy669 <59546836+Yzxy669@users.noreply.github.com>
2024-08-16 10:40:07 +0800

    Add files via upload

---
+++ ALSLHASS/iteration_condition.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+before_pre_label = 0
+
+
+def whether_iteration(predict_label, iter_num):
+    global before_pre_label
+    if iter_num == 0:
+        before_pre_label = predict_label
+        return 0
+    else:
+        current_pre_label = np.array(predict_label)
+        equal_prob = sum(current_pre_label != before_pre_label) / len(before_pre_label)
+        before_pre_label = np.array(predict_label)
+        return equal_prob
