5580b8090990a901cd7df9d102971fee10a6a627
Yzxy669 <59546836+Yzxy669@users.noreply.github.com>
2024-08-22 12:24:41 +0800

    Update iteration_condition.py

--- ALSLHASS/iteration_condition.py
+++ ALSLHASS/iteration_condition.py
@@ -7,9 +7,9 @@ def whether_iteration(predict_label, iter_num):
     global before_pre_label
     if iter_num == 0:
         before_pre_label = predict_label
-        return 0
+        return 0.0
     else:
         current_pre_label = np.array(predict_label)
-        equal_prob = format(sum(current_pre_label != before_pre_label) / len(before_pre_label), '.3f')
+        equal_prob = round(sum(current_pre_label != before_pre_label) / len(before_pre_label), 3)
         before_pre_label = np.array(predict_label)
         return equal_prob
