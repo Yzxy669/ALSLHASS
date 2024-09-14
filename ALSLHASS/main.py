ded7b07bd911e8ac6cccdea5d3f40af147157854
Yzxy669 <59546836+Yzxy669@users.noreply.github.com>
2024-09-14 14:17:01 +0800

    Update main.py

--- ALSLHASS/main.py
+++ ALSLHASS/main.py
@@ -14,23 +14,17 @@ if __name__ == '__main__':
     Train_iterNum = 'Train-%s' % iter_num
     # 加载训练数据
     isbi_dataset = dp.ISBI_Loader(data_path, Train_iterNum, transform=torchvision.transforms.ToTensor())
-    train_loader = torch.utils.data.DataLoader(dataset=isbi_dataset,
-                                               batch_size=16,
-                                               shuffle=True)
+    train_loader = torch.utils.data.DataLoader(dataset=isbi_dataset, batch_size=32, shuffle=True)
 
     isbi_dataset = dp.ISBI_Loader(data_path, 'ValidSet', transform=torchvision.transforms.ToTensor())
-    val_loader = torch.utils.data.DataLoader(dataset=isbi_dataset,
-                                             batch_size=256,
-                                             shuffle=False)
+    val_loader = torch.utils.data.DataLoader(dataset=isbi_dataset, batch_size=256, shuffle=False)
 
     # 模型
     print("开始训练.....")
     aff_resnet_model = cross_train_verifi(train_loader, class_num, val_loader, data_path, iter_num)
 
     isbi_dataset = dp.ISBI_Loader(data_path, 'Test', transform=torchvision.transforms.ToTensor())
-    test_loader = torch.utils.data.DataLoader(dataset=isbi_dataset,
-                                              batch_size=256,
-                                              shuffle=False)
+    test_loader = torch.utils.data.DataLoader(dataset=isbi_dataset, batch_size=256, shuffle=False)
     print("开始测试.....")
     guide_feature, pre_class_path, features_list, _ = predict(aff_resnet_model, test_loader, class_num, iter_num,
                                                               data_path)
