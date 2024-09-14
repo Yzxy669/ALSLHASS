d93605b8f8f9c9c0b5a10010d75685de8ce843c6
Yzxy669 <59546836+Yzxy669@users.noreply.github.com>
2024-09-14 14:14:47 +0800

    Update aff_resnet.py

--- ALSLHASS/aff_resnet.py
+++ ALSLHASS/aff_resnet.py
@@ -2,10 +2,6 @@ import torch
 import torch.nn as nn
 from fusion import AFF, iAFF, DAF
 
-__all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
-           'resnet152', 'resnext50_32x4d', 'resnext101_32x8d',
-           'wide_resnet50_2', 'wide_resnet101_2']
-
 
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
     """3x3 convolution with padding"""
@@ -130,7 +126,7 @@ class Bottleneck(nn.Module):
 
 class ResNet(nn.Module):
 
-    def __init__(self, block, layers, num_classes=1000, zero_init_residual=False,
+    def __init__(self, block, layers=(2, 2, 2, 2), num_classes=1000, zero_init_residual=False,
                  groups=1, width_per_group=64, replace_stride_with_dilation=None,
                  norm_layer=None, fuse_type='DAF', small_input=False):
         super(ResNet, self).__init__()
@@ -223,10 +219,10 @@ class ResNet(nn.Module):
         x = self.relu(x)
 
         x = self.layer1(x)
-        #x = self.dropout(x)
+        # x = self.dropout(x)
 
         x = self.layer2(x)
-        #x = self.dropout(x)
+        # x = self.dropout(x)
 
         x = self.layer3(x)
         x = self.dropout(x)
@@ -248,134 +244,16 @@ class ResNet(nn.Module):
         return self._forward_impl(x)
 
 
-def _resnet(arch, block, layers, num_classes, fuse_type, small_input, **kwargs):
-    model = ResNet(block, layers, num_classes=num_classes, fuse_type=fuse_type, small_input=small_input, **kwargs)
+def _resnet(block, num_classes, fuse_type, small_input, **kwargs):
+    model = ResNet(block, num_classes=num_classes, fuse_type=fuse_type, small_input=small_input, **kwargs)
     return model
 
 
-def resnet18(num_classes, fuse_type='DAF', **kwargs):
-    r"""ResNet-18 model from
-    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
-
+def aff_net(num_classes, fuse_type='DAF', **kwargs):
+    r"""
     Args:
         pretrained (bool): If True, returns a model pre-trained on ImageNet
         progress (bool): If True, displays a progress bar of the download to stderr
     """
-    return _resnet('resnet18', BasicBlock, [2, 2, 2, 2], num_classes, fuse_type, **kwargs)
+    return _resnet(BasicBlock, num_classes, fuse_type, **kwargs)
 
-
-def resnet34(num_classes, fuse_type='DAF', **kwargs):
-    r"""ResNet-34 model from
-    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    return _resnet('resnet34', BasicBlock, [3, 4, 6, 3], num_classes, fuse_type, **kwargs)
-
-
-def resnet50(num_classes, fuse_type='DAF', small_input=False, **kwargs):
-    r"""ResNet-50 model from
-    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    return _resnet('resnet50', Bottleneck, [3, 4, 6, 3], num_classes, fuse_type, small_input, **kwargs)
-
-
-def resnet101(fuse_type='DAF', small_input=False, **kwargs):
-    r"""ResNet-101 model from
-    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    return _resnet('resnet101', Bottleneck, [3, 4, 23, 3], fuse_type, small_input, **kwargs)
-
-
-def resnet152(fuse_type='DAF', small_input=False, **kwargs):
-    r"""ResNet-152 model from
-    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    return _resnet('resnet152', Bottleneck, [3, 8, 36, 3], fuse_type, small_input, **kwargs)
-
-
-def resnext50_32x4d(fuse_type='DAF', small_input=False, **kwargs):
-    r"""ResNeXt-50 32x4d model from
-    `"Aggregated Residual Transformation for Deep Neural Networks" <https://arxiv.org/pdf/1611.05431.pdf>`_
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    kwargs['groups'] = 32
-    kwargs['width_per_group'] = 4
-    return _resnet('resnext50_32x4d', Bottleneck, [3, 4, 6, 3], fuse_type, small_input, **kwargs)
-
-
-def resnext101_32x8d(fuse_type='DAF', small_input=False, **kwargs):
-    r"""ResNeXt-101 32x8d model from
-    `"Aggregated Residual Transformation for Deep Neural Networks" <https://arxiv.org/pdf/1611.05431.pdf>`_
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    kwargs['groups'] = 32
-    kwargs['width_per_group'] = 8
-    return _resnet('resnext101_32x8d', Bottleneck, [3, 4, 23, 3], fuse_type, small_input, **kwargs)
-
-
-def wide_resnet50_2(fuse_type='DAF', small_input=False, **kwargs):
-    r"""Wide ResNet-50-2 model from
-    `"Wide Residual Networks" <https://arxiv.org/pdf/1605.07146.pdf>`_
-
-    The model is the same as ResNet except for the bottleneck number of channels
-    which is twice larger in every block. The number of channels in outer 1x1
-    convolutions is the same, e.g. last block in ResNet-50 has 2048-512-2048
-    channels, and in Wide ResNet-50-2 has 2048-1024-2048.
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    kwargs['width_per_group'] = 64 * 2
-    return _resnet('wide_resnet50_2', Bottleneck, [3, 4, 6, 3], fuse_type, small_input, **kwargs)
-
-
-def wide_resnet101_2(fuse_type='DAF', small_input=False, **kwargs):
-    r"""Wide ResNet-101-2 model from
-    `"Wide Residual Networks" <https://arxiv.org/pdf/1605.07146.pdf>`_
-
-    The model is the same as ResNet except for the bottleneck number of channels
-    which is twice larger in every block. The number of channels in outer 1x1
-    convolutions is the same, e.g. last block in ResNet-50 has 2048-512-2048
-    channels, and in Wide ResNet-50-2 has 2048-1024-2048.
-
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
-    kwargs['width_per_group'] = 64 * 2
-    return _resnet('wide_resnet101_2', Bottleneck, [3, 4, 23, 3], fuse_type, small_input, **kwargs)
-
-
-if __name__ == '__main__':
-    import os
-
-    os.environ['CUDA_VISIBLE_DEVICES'] = "1,0"
-    # AFF,iAFF,DAF
-
-    # [batch,channel,H,W]
-    img = torch.rand(2, 3, 112, 112).cuda()
-    model = wide_resnet101_2(fuse_type='DAF', small_input=False).cuda().train()
-    result = model(img)
-    print(result.shape)
